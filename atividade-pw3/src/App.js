b0f8e32a0b8bbc391fdac518baab35f6db41f67a
muffliv <livymendes5@gmail.com>
2024-02-26 21:55:52 -0300

    Estilização e Criação de Modal

--- atividade-pw3/src/App.js
+++ atividade-pw3/src/App.js
@@ -3,26 +3,80 @@ import CardBook from './components/CardBooks';
 import booksData from './livros.json'
 import Header from './components/Header';
 import './App.css'
+import { useState } from 'react';
 
 
 function App() {
+    const [titulo, setTitulo] = useState(null)
+    const [imagem, setImagem] = useState(null)
+    const [autor, setAutor] = useState(null)
+    const [descricao, setDescricao] = useState(null)
+    const [avaliacao, setAvaliacao] = useState(null)
 
+    const[efeito, setEfeito] = useState("home")
+    const[open, setOpen] = useState("livro-modal close")
+  
+  function bookdetails(imagem, titulo, autor, descricao, avaliacao) {
+    setOpen("livro-modal open")
+    setEfeito("home efeito")
 
+    setTitulo(titulo)
+    setImagem(imagem)
+    setAutor(autor)
+    setDescricao(descricao)
+    setAvaliacao(avaliacao)
+  }
+
+  function fecharModal(){
+    setEfeito("home")
+    setOpen("livro-modal close")
+  }
 
     return (
-      <div className="livro-list-container">
-        <Header/>
-        {booksData.map((livro, index) => (
-          <CardBook
-            key={index}
-            titulo={livro.titulo}
-            autor={livro.autor}
-            imagem={livro.imagem} className="imagem-card"
-            descricao={livro.descricao}
-            avaliacao={livro.avaliacao}
-          />
-        ))}
-      </div>
+      <>
+        <div className={efeito}>
+          <Header/>
+          <div className="livro-list-container">    
+            {
+              booksData.map((livro, index) => {    
+                return(
+                  <div onClick={() => bookdetails(livro.imagem, livro.titulo,livro.autor, livro.descricao, livro.avaliacao )}>
+                    <CardBook
+                      key={index}
+                      titulo={livro.titulo}
+                      autor={livro.autor}
+                      imagem={livro.imagem} className="imagem-card"
+                      descricao={livro.descricao}
+                      avaliacao={livro.avaliacao}
+
+                      />
+                  </div>
+                )    
+              })}
+          </div>
+        </div>
+        <div className={open}>
+          <header>
+            <button className='livro-modal__btn' onClick={fecharModal}>fechar</button>
+          </header>
+          <div className='livro-modal__conteudo'>
+            <div className='livro-modal__esquerdo'>
+              <img src={imagem}/>
+              <p>{avaliacao}</p>
+            </div>
+
+            <div className='livro-modal__direito'>
+              <h1>{titulo}</h1>
+              <h2>{autor}</h2>
+              <p>{descricao}</p>
+
+            </div>
+          </div>
+          
+          
+           
+        </div>
+      </>
     );
 };
 
--- atividade-pw3/src/components/Header.js
+++ atividade-pw3/src/components/Header.js
@@ -3,7 +3,9 @@ import '../App.css'
 function Header(){
   return(
     <div className="header">
-      <h1>Books</h1>
+      <h1>
+        <img className= "logo" src='https://freelogopng.com/images/all_img/1688364164amazon-logo-transparent.png' width={100}/>
+      </h1>
     </div>
   )
 };
